00704d6d4bd19de5392240a1d6b09c519dcf57e2
Mégane Carême <meganecareme1@gmail.com>
2022-12-22 10:35:50 +0100

    add utilisateur component list

--- certif/src/app/modules/utilisateur/list/list.component.ts
+++ certif/src/app/modules/utilisateur/list/list.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { UtilisateurService } from 'src/app/services/utilisateur.service';
 
 @Component({
   selector: 'utilisateur-list',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent {
+
+  public utilisateurs: any[] = [];
+
+  constructor(
+    private utilisateurService: UtilisateurService,
+    private httpClient: HttpClient
+  ) {}
+
+  ngOnInit(): void {
+    this.utilisateurService.utilisateurs.subscribe(data => this.utilisateurs = data);
+  }
 
 }
